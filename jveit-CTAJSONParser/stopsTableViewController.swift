fa8c672554f8d4fb88dff2f77ee0c79c89aa7fe8
Jeffrey Veit <jveit1186@gmail.com>
2021-04-21 13:33:19 -0700

    second

--- jveit-CTAJSONParser/stopsTableViewController.swift
+++ jveit-CTAJSONParser/stopsTableViewController.swift
@@ -32,6 +32,18 @@ class stopsTableViewController: UITableViewController {
                 return blue.count
             case "Red":
                 return red.count
+            case "Orange":
+                return orange.count
+            case "Yellow":
+                return yellow.count
+            case "Purple":
+                return purple.count
+            case "Pink":
+                return pink.count
+            case "Brown":
+                return brown.count
+            case "Green":
+                return green.count
             default:
                 return 1
         }
@@ -60,6 +72,48 @@ class stopsTableViewController: UITableViewController {
                 cell.textLabel?.text = item.name
                 return cell
                 
+            case "Orange":
+                let item = orange[indexPath.row]
+                let cell = tableView.dequeueReusableCell(withIdentifier: item.type.rawValue, for: indexPath)
+                print(item.name)
+                cell.textLabel?.text = item.name
+                return cell
+                
+            case "Yellow":
+                let item = yellow[indexPath.row]
+                let cell = tableView.dequeueReusableCell(withIdentifier: item.type.rawValue, for: indexPath)
+                print(item.name)
+                cell.textLabel?.text = item.name
+                return cell
+                
+            case "Purple":
+                let item = purple[indexPath.row]
+                let cell = tableView.dequeueReusableCell(withIdentifier: item.type.rawValue, for: indexPath)
+                print(item.name)
+                cell.textLabel?.text = item.name
+                return cell
+                
+            case "Pink":
+                let item = pink[indexPath.row]
+                let cell = tableView.dequeueReusableCell(withIdentifier: item.type.rawValue, for: indexPath)
+                print(item.name)
+                cell.textLabel?.text = item.name
+                return cell
+                
+            case "Brown":
+                let item = brown[indexPath.row]
+                let cell = tableView.dequeueReusableCell(withIdentifier: item.type.rawValue, for: indexPath)
+                print(item.name)
+                cell.textLabel?.text = item.name
+                return cell
+                
+            case "Green":
+                let item = green[indexPath.row]
+                let cell = tableView.dequeueReusableCell(withIdentifier: item.type.rawValue, for: indexPath)
+                print(item.name)
+                cell.textLabel?.text = item.name
+                return cell
+                
             default:
                 let item = blue[indexPath.row]
                 let cell = tableView.dequeueReusableCell(withIdentifier: item.type.rawValue, for: indexPath)
@@ -70,11 +124,41 @@ class stopsTableViewController: UITableViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let color = itemLine?.name
+        
         if let detailViewController = segue.destination as? detailViewController {
-           if let indexPath = self.tableView.indexPathForSelectedRow {
-            detailViewController.mapId = blue[indexPath.row]
+           if let indexPath = self.tableView.indexPathForSelectedRow
+           {
+            switch color
+            {
+                case "Blue":
+                    detailViewController.mapId = blue[indexPath.row]
+                    
+                case "Red":
+                    detailViewController.mapId = red[indexPath.row]
+                    
+                case "Orange":
+                    detailViewController.mapId = orange[indexPath.row]
+                    
+                case "Yellow":
+                    detailViewController.mapId = yellow[indexPath.row]
+                    
+                case "Purple":
+                    detailViewController.mapId = purple[indexPath.row]
+                    
+                case "Pink":
+                    detailViewController.mapId = pink[indexPath.row]
+                    
+                case "Brown":
+                    detailViewController.mapId = brown[indexPath.row]
+                    
+                case "Green":
+                    detailViewController.mapId = green[indexPath.row]
+                
+                default:
+                    detailViewController.mapId = blue[indexPath.row]
+            }
+            
            }
         }
     }
--- jveit-CTAJSONParser/trainStops.swift
+++ jveit-CTAJSONParser/trainStops.swift
@@ -145,16 +145,288 @@ let blue =
 
 let red =
     [
-        stops(name: "Washington",
+        stops(name: "Howard",
               type: .red,
-              mapId: "40550"
+              mapId: "40900"
+            ),
+        stops(name: "Jarvis",
+              type: .red,
+              mapId: "41190"
+            ),
+        stops(name: "Morse",
+              type: .red,
+              mapId: "40100"
+            ),
+        stops(name: "Loyola",
+              type: .red,
+              mapId: "41300"
+            ),
+        stops(name: "Granville",
+              type: .red,
+              mapId: "40760"
+            ),
+        stops(name: "Thorndale",
+              type: .red,
+              mapId: "40880"
+            ),
+        stops(name: "Bryn Mawr",
+              type: .red,
+              mapId: "41380"
+            ),
+        stops(name: "Berwyn",
+              type: .red,
+              mapId: "40340"
+            ),
+        stops(name: "Argyle",
+              type: .red,
+              mapId: "41200"
+            ),
+        stops(name: "Lawrence",
+              type: .red,
+              mapId: "40770"
+            ),
+        stops(name: "Wilson",
+              type: .red,
+              mapId: "40540"
+            ),
+        stops(name: "Sheridan",
+              type: .red,
+              mapId: "40080"
+            ),
+        stops(name: "Addison",
+              type: .red,
+              mapId: "41420"
+            ),
+        stops(name: "Belmont",
+              type: .red,
+              mapId: "41320"
+            ),
+        stops(name: "Fullerton",
+              type: .red,
+              mapId: "41220"
+            ),
+        stops(name: "North/Clybourn",
+              type: .red,
+              mapId: "40650"
+            ),
+        stops(name: "Clark/Division",
+              type: .red,
+              mapId: "40630"
+            ),
+        stops(name: "Chicago",
+              type: .red,
+              mapId: "41450"
+            ),
+        stops(name: "Grand",
+              type: .red,
+              mapId: "40330"
+            ),
+        stops(name: "Lake",
+              type: .red,
+              mapId: "41660"
+            ),
+        stops(name: "Monroe",
+              type: .red,
+              mapId: "41090"
             ),
         stops(name: "Jackson",
               type: .red,
               mapId: "40560"
+            ),
+        stops(name: "Harrison",
+              type: .red,
+              mapId: "41490"
+            ),
+        stops(name: "Roosevelt",
+              type: .red,
+              mapId: "41400"
+            ),
+        stops(name: "Cermak-Chinatown",
+              type: .red,
+              mapId: "41000"
+            ),
+        stops(name: "Sox-35th",
+              type: .red,
+              mapId: "40190"
+            ),
+        stops(name: "47th",
+              type: .red,
+              mapId: "41230"
+            ),
+        stops(name: "Garfield",
+              type: .red,
+              mapId: "41170"
+            ),
+        stops(name: "63rd",
+              type: .red,
+              mapId: "40910"
+            ),
+        stops(name: "69th",
+              type: .red,
+              mapId: "40990"
+            ),
+        stops(name: "79th",
+              type: .red,
+              mapId: "40240"
+            ),
+        stops(name: "87th",
+              type: .red,
+              mapId: "41430"
+            ),
+        stops(name: "95th",
+              type: .red,
+              mapId: "40450"
             )
     ]
 
+    let orange =
+    [
+        stops(name: "Midway",
+              type: .orange,
+              mapId: "40930"
+            ),
+        stops(name: "Pulaski",
+              type: .orange,
+              mapId: "40960"
+            ),
+        stops(name: "Kedzie",
+              type: .orange,
+              mapId: "41150"
+            ),
+        stops(name: "Western",
+              type: .orange,
+              mapId: "40310"
+            ),
+        stops(name: "35th/Archer",
+              type: .orange,
+              mapId: "40120"
+            ),
+        stops(name: "Ashland",
+              type: .orange,
+              mapId: "41060"
+            ),
+        stops(name: "Halsted",
+              type: .orange,
+              mapId: "41130"
+            ),
+        stops(name: "Roosevelt",
+              type: .orange,
+              mapId: "41400"
+            ),
+        stops(name: "Harold Washington Library",
+              type: .orange,
+              mapId: "40850"
+            ),
+        stops(name: "LaSalle/Van Buren",
+              type: .orange,
+              mapId: "40160"
+            ),
+        stops(name: "Quincy/Wells",
+              type: .orange,
+              mapId: "40040"
+            ),
+        stops(name: "Washinton/Wells",
+              type: .orange,
+              mapId: "40730"
+            ),
+        stops(name: "Clark/Lake",
+              type: .orange,
+              mapId: "40380"
+            ),
+        stops(name: "State/Lake",
+              type: .orange,
+              mapId: "40260"
+            ),
+        stops(name: "Washington/Wabash",
+              type: .orange,
+              mapId: "41700"
+            ),
+        stops(name: "Adams/Wabash",
+              type: .orange,
+              mapId: "40680"
+            )
+    ]
+
+   let yellow =
+    [
+        stops(name: "Howard",
+              type: .yellow,
+              mapId: "40900"
+            ),
+        stops(name: "Oakton-Skokie",
+              type: .yellow,
+              mapId: "41680"
+            ),
+        stops(name: "Dempster-Skokie",
+              type: .yellow,
+              mapId: "40140"
+            )
+    ]
+
+    let purple =
+    [
+        stops(name: "Linden",
+              type: .purple,
+              mapId: "41050"
+            ),
+        stops(name: "Central",
+              type: .purple,
+              mapId: "41250"
+            ),
+        stops(name: "Noyes",
+              type: .purple,
+              mapId: "40400"
+            )
+        
+    ]
+
+    let pink =
+    [
+        stops(name: "54th/Cermak",
+              type: .pink,
+              mapId: "40580"
+            ),
+        stops(name: "Cicero/Cermak",
+              type: .pink,
+              mapId: "40420"
+            ),
+        stops(name: "Kostner",
+              type: .pink,
+              mapId: "40600"
+            )
+    ]
+
+    let brown =
+    [
+        stops(name: "Kimball",
+              type: .brown,
+              mapId: "41290"
+            ),
+        stops(name: "Kedzie",
+              type: .brown,
+              mapId: "41180"
+            ),
+        stops(name: "Francisco",
+              type: .brown,
+              mapId: "40870"
+            )
+    ]
+
+    let green =
+    [
+        stops(name: "Harlem",
+              type: .green,
+              mapId: "40020"
+            ),
+        stops(name: "Oak Park",
+              type: .green,
+              mapId: "41350"
+            ),
+        stops(name: "Ridgeland",
+              type: .green,
+              mapId: "40610"
+            )
+    ]
 
 class stops
 {
